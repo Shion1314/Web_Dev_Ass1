05f3eb457939ac2baf951000f723a35330950858
Shion1314 <jiaxinlin200204@gmail.com>
2023-09-18 19:03:28 -0400

    Adding .js file for both html file and implementing button features

--- Sion.js
+++ Sion.js
@@ -1,1 +1,9 @@
-const button = documentElementById("shion_id")
+const button = document.getElementById("Sion_id");
+button.addEventListener("click",()=>{
+    alert("It's Sion!!!");
+})
+
+const button_2 = document.getElementById("Rio_id");
+button_2.addEventListener("click",()=>{
+    alert("It's Rio!!!");
+})
